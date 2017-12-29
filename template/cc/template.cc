c1c332f12d1b144229953f08df536d729c710e29
himkt <himkt@klis.tsukuba.ac.jp>
2017-12-29 19:25:25 +0900

    Add lcm into cc template

--- template/cc/template.cc
+++ template/cc/template.cc
@@ -3,6 +3,11 @@
 # define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 
+template<typename integer>
+integer __lcm(integer a, integer b) {
+  return (a * b) / __gcd(a, b);
+}
+
 int main() {
   return 0;
 }
