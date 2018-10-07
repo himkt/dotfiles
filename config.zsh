a4297b67d1739e6194573bd7d9ff1cb5656467c8
makoto-hiramatsu <makoto-hiramatsu@cookpad.com>
2018-10-07 22:50:09 +0900

    Separate git config for zsh from .zshrc

--- config.zsh
+++ config.zsh
@@ -3,16 +3,6 @@
 #
 
 
-function load() {
-  [ -f $1 ] && source $1
-}
-
-
-function cd() {
-  builtin cd $@ && ls;
-}
-
-
 export LANG=ja_JP.UTF-8
 export LC_CTYPE=ja_JP.UTF-8
 
@@ -82,7 +72,6 @@ fi
 # autoloads
 autoload -Uz colors; colors
 autoload -Uz zmv
-autoload -Uz vcs_info
 autoload -Uz add-zsh-hook
 autoload -Uz is-at-least
 autoload -U compinit; compinit
@@ -203,68 +192,13 @@ alias bruby="bundle exec ruby"
 alias g++="g++ --std=c++11 -O3 -Wall -I$(brew --prefix)/include"
 
 
-# Modify the colors and symbols in these variables as desired.
-GIT_PROMPT_SYMBOL="%{$fg[blue]%} ± "
-GIT_PROMPT_PREFIX="%{$fg[green]%}[%{$reset_color%}"
-GIT_PROMPT_SUFFIX="%{$fg[green]%}]%{$reset_color%}"
-GIT_PROMPT_AHEAD="%{$fg[red]%}ANUM%{$reset_color%}"
-GIT_PROMPT_BEHIND="%{$fg[cyan]%}BNUM%{$reset_color%}"
-GIT_PROMPT_MERGING="%{$fg[magenta]%}⚡︎%{$reset_color%}"
-GIT_PROMPT_UNTRACKED="%{$fg[red]%} ● %{$reset_color%}"
-GIT_PROMPT_MODIFIED="%{$fg[yellow]%} ● %{$reset_color%}"
-GIT_PROMPT_STAGED="%{$fg[green]%} ● %{$reset_color%}"
-
-
-# Show Git branch/tag, or name-rev if on detached head
-function parse_git_branch() {
-  (git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
+function load() {
+  [ -f $1 ] && source $1
 }
 
 
-# Show different symbols as appropriate for various Git repository states
-function parse_git_state() {
-
-  # Compose this value via multiple conditional appends.
-  local GIT_STATE=""
-
-  local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
-  if [ "$NUM_AHEAD" -gt 0 ]; then
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
-  fi
-
-  local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
-  if [ "$NUM_BEHIND" -gt 0 ]; then
-    GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
-  fi
-
-  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
-  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
-  fi
-
-  if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
-  fi
-
-  if ! git diff --quiet 2> /dev/null; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
-  fi
-
-  if ! git diff --cached --quiet 2> /dev/null; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
-  fi
-
-  if [[ -n $GIT_STATE ]]; then
-    echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"
-  fi
-
-}
-
-
-# If inside a Git repository, print its branch and state
-function git_prompt_string() {
-  local git_where="$(parse_git_branch)"
-  [ -n "$git_where" ] && echo "$GIT_PROMPT_SYMBOL$(parse_git_state)$GIT_PROMPT_PREFIX%{$fg[yellow]%}${git_where#(refs/heads/|tags/)}$GIT_PROMPT_SUFFIX"
+function cd() {
+  builtin cd $@ && ls;
 }
 
 
@@ -279,20 +213,8 @@ function ssh() {
 }
 
 
-# Set the right-hand prompt
-# NOTE: do not use double quotation
-PROMPT='%{${fg[yellow]}%}[ %~ ]%{${reset_color}%}'  # filepath
-PROMPT+=' @ '
-PROMPT+='%{${fg[cyan]}%}%m%{${reset_color}%}'  # hostname
-PROMPT+=' '
-PROMPT+=`echo -e "%{$fg_bold[blue]%}%D{%m/%f} -- %D{%K:%M:%S}%{${reset_color}%}"`  # timestamp
-PROMPT+=$'\n'
-PROMPT+='$ '
-RPROMPT='$(git_prompt_string)'
-SPROMPT="%{${fg[red]}%}%r is correct? [y, n, a, e]:%{${reset_color}%}"
-
-
 load $HOME/.dotfiles/private/private.zsh
+load $HOME/.dotfiles/submodule/zsh-git-prompt/zsh-git-prompt.zsh
 load $HOME/.dotfiles/submodule/zsh-syntax-highlighting.git/zsh-syntax-highlighting.zsh
 load $HOME/.dotfiles/submodule/zsh-history-substring-search.git/zsh-history-substring-search.zsh
 load $HOME/.dotfiles/submodule/z.git/z.sh
