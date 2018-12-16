82cc99047bd75ee8207ebeb01cbc693ceffd375d
makoto-hiramatsu <makoto-hiramatsu@cookpad.com>
2018-12-16 21:20:43 +0900

    Add installer

---
+++ bin/installer.sh
@@ -0,0 +1,20 @@
+!/bin/sh
+
+
+# check git command
+type git || {
+  echo 'Please install git or update your path to include the git executable!'
+  exit 1
+}
+echo ""
+
+
+echo "clone .dotfiles..."
+git clone git@github.com:himkt/.dotfiles.git $HOME/.dotfiles
+
+
+echo "installing..."
+cd $HOME/.dotfiles && make
+
+
+echo "finished."
