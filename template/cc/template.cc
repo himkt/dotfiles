fbfcf2d86d326bbb7b588a0bcfc04e4f9c88d0b4
himkt <himkt@klis.tsukuba.ac.jp>
2019-01-27 21:43:14 +0900

    Update cc template for a programming contest

--- template/cc/template.cc
+++ template/cc/template.cc
@@ -1,5 +1,37 @@
-//#define _GRIBCXX_DEBUG
-#include <bits/stdc++.h>
+// include headers{{{
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+/*}}}*/
 # define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 
