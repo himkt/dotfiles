b8d2791de7e9555face5269b64169e399fa3868c
himkt <himkt@klis.tsukuba.ac.jp>
2019-02-09 22:15:12 +0900

    Update the installer

--- bin/installer.sh
+++ bin/installer.sh
@@ -3,26 +3,30 @@
 
 # check git command
 type git || {
-  echo 'Please install git or update your path to include the git executable!'
+  echo -e "Please install git or update your path to include the git executable!"
   exit 1
 }
 
 
-echo "Clone dotfiles..."
+echo -e "Clone dotfiles..."
 git clone git@github.com:himkt/dotfiles.git $HOME/.dotfiles
 
 
-echo "Installing dotfiles..."
+echo -e "Installing dotfiles..."
 cd $HOME/.dotfiles && make
 
 
-source $HOME/.zshrc
-echo "Finished!"
-echo "- If you want to use neovim with extensions,"
-echo "  please run `make requirements`."
-echo "  (`make requirements` needs pyenv)"
-echo "--------------------------------------------"
-echo "- If you want to use linuxbrew/homebrew,"
-echo "  please run `make brew_bundle`."
-echo "  Please run `make build_brew` before"
-echo "  if you have not installed brew yet"
+echo -e ""
+echo -e "### Finish installing dotfiles!"
+echo -e "Please run \e[31msource $HOME/.zshrc\e[0m to enable configures."
+echo -e "- If you want to use neovim with extensions,"
+echo -e "  please run \e[31mmake requirements\e[0m (which needs pyenv)."
+echo -e ""
+echo -e "---"
+echo -e ""
+echo -e "### Packages"
+echo -e "- If you want to use linuxbrew/homebrew,"
+echo -e "  please run \e[31mmake brew_bundle\e[0m."
+echo -e "  Please run \e[31mmake build_brew\e[0m before"
+echo -e "  if you have not installed brew yet"
+echo -e ""
