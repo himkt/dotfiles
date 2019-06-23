1e7e7d5454e61ae91518e02e6b82623dc415fff1
himkt <himkt@klis.tsukuba.ac.jp>
2019-06-24 07:58:06 +0900

    Update template for c++

--- template/cc/template.cc
+++ template/cc/template.cc
@@ -1,39 +1,16 @@
-// include headers{{{
 #include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
 #include <iostream>
 #include <istream>
 #include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
 #include <queue>
 #include <set>
-#include <sstream>
 #include <stack>
-#include <stdexcept>
-#include <streambuf>
 #include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
 #include <vector>
-/*}}}*/
-# define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+
 using namespace std;
+
 
 int main() {
     return 0;
