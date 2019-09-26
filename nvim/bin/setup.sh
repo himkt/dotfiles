4ef68d1bf912d2acbd12c8d3b14cd49295c82056
himkt <himkt@cookpad.com>
2019-09-26 22:54:17 +0900

    Install coc.vim

--- nvim/bin/setup.sh
+++ nvim/bin/setup.sh
@@ -1,12 +1,11 @@
 #! /bin/sh
 
 mkdir -p $HOME/.config/nvim
-ln -s `pwd`/nvim/config.d/init.vim $HOME/.config/nvim/init.vim
+ln -s $HOME/.dotfiles/nvim/config.d/init.vim $HOME/.config/nvim/init.vim
+ln -s $HOME/.dotfiles/nvim/config.d/coc-settings.json $HOME/.config/nvim/coc-settings.json
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-pip3 install neovim black isort autopep8 flake8 --user
 nvim -u $(pwd)/nvim/config.d/tiny.init.vim +PlugInstall +qall
-nvim -u $(pwd)/nvim/config.d/init.vim +UpdateRemotePlugins +qall
 echo 'finish creating the neovim environment!'
