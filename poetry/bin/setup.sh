e5ee2fd55dc49859b0fa4a74e028bb5246818c4b
himkt <himkt@cookpad.com>
2019-12-21 23:43:48 +0900

    Add double quotes

--- poetry/bin/setup.sh
+++ poetry/bin/setup.sh
@@ -3,14 +3,14 @@
 
 case ${OSTYPE} in
   darwin*)
-    POETRY_HOME=$HOME/Library/ApplicationSupport/pypoetry
+    POETRY_HOME="$HOME/Library/Application\\ Support/pypoetry"
     ;;
   linux*)
-    POETRY_HOME=$HOME/.config/pypoetry
+    POETRY_HOME="$HOME/.config/pypoetry"
     ;;
 esac
 
 
-mkdir -p $POETRY_HOME
-rm -f $POETRY_HOME/config.toml
-ln -s `pwd`/poetry/config.d/config.toml $POETRY_HOME/config.toml
+mkdir -p "$POETRY_HOME"
+rm -f "$POETRY_HOME/config.toml"
+ln -s "`pwd`/poetry/config.d/config.toml" "$POETRY_HOME/config.toml"
