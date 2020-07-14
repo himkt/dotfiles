31873aa9594c331b05dd16f005ed1b78a61500a1
himkt <himkt@cookpad.com>
2020-07-15 00:39:04 +0900

    Cleanup config.fish

--- fish/config.d/config.fish
+++ fish/config.d/config.fish
@@ -4,36 +4,7 @@
 # @author: himkt
 #
 
-set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_hide_untrackedfiles 1
-
-set -g __fish_git_prompt_color_branch magenta
-set -g __fish_git_prompt_showupstream "informative"
-set -g __fish_git_prompt_char_upstream_ahead "↑"
-set -g __fish_git_prompt_char_upstream_behind "↓"
-set -g __fish_git_prompt_char_upstream_prefix ""
-
-set -g __fish_git_prompt_char_stagedstate " ● "
-set -g __fish_git_prompt_char_dirtystate " ● "
-set -g __fish_git_prompt_char_untrackedfiles " ● "
-set -g __fish_git_prompt_char_conflictedstate " ✖ "
-set -g __fish_git_prompt_char_cleanstate " ✔ "
-
-set -g __fish_git_prompt_color_dirtystate $fish_color_param
-set -g __fish_git_prompt_color_stagedstate $fish_color_host_remote
-set -g __fish_git_prompt_color_invalidstate $fish_color_root
-set -g __fish_git_prompt_color_untrackedfiles $fish_color_cwd_root
-set -g __fish_git_prompt_color_cleanstate $fish_color_cwd
-
-function fish_prompt
-  printf '%s[ %s ]%s @ %s%s%s %s\n%s%s> ' \
-    (set_color $fish_color_cwd) (prompt_pwd) (set_color $fish_color_host_remote) \
-    (hostname| cut -d . -f1) (set_color $fish_color_host_remote) (set_color $fish_color_operator) (date '+%m/%d %H:%M:%S') (set_color normal)
-end
-
-function fish_right_prompt
-  echo -n (fish_git_prompt)
-end
+bind \cs 'fzf-z-search'
 
 # env
 set -Ux EDITOR emacs
@@ -41,6 +12,25 @@ set -Ux GOPATH $HOME/go
 set -Ux LANG en_US.UTF-8
 set -Ux XDG_CONFIG_HOME $HOME/.config
 set -Ux PYTHONDONTWRITEBYTECODE 1
+set -Ux fish_color_search_match --background=$fish_color_selection
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+set -g __fish_git_prompt_color_branch magenta
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+set -g __fish_git_prompt_char_stagedstate " ● "
+set -g __fish_git_prompt_char_dirtystate " ● "
+set -g __fish_git_prompt_char_untrackedfiles " ● "
+set -g __fish_git_prompt_char_conflictedstate " ✖ "
+set -g __fish_git_prompt_char_cleanstate " ✔ "
+set -g __fish_git_prompt_color_dirtystate $fish_color_param
+set -g __fish_git_prompt_color_stagedstate $fish_color_host_remote
+set -g __fish_git_prompt_color_invalidstate $fish_color_root
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_cwd_root
+set -g __fish_git_prompt_color_cleanstate $fish_color_cwd
 
 # brew
 for brew_prefix in "/opt/brew" "$HOME/.linuxbrew" "/home/linuxbrew/.linuxbrew"
@@ -73,17 +63,14 @@ function attach_or_create_tmux_session
   if set -q TERM_PROGRAM; and test $TERM_PROGRAM = "vscode"
     return
   end
-  echo "not vscode"
 
   if set -q DISABLE_TMUX
     return
   end
-  echo "not disabled"
 
   if set -q TMUX_PANE
     return
   end
-  echo "not tmux_pane"
 
   if tmux has-session
     tmux attach
@@ -106,6 +93,17 @@ end
 # GHQ
 if type -q ghq
   set -Ux GHQ_ROOT $HOME/work
+end
+
+# prompt
+function fish_prompt
+  printf '%s[ %s ]%s @ %s%s%s %s\n%s%s> ' \
+    (set_color $fish_color_cwd) (prompt_pwd) (set_color $fish_color_host_remote) \
+    (hostname| cut -d . -f1) (set_color $fish_color_host_remote) (set_color $fish_color_operator) (date '+%m/%d %H:%M:%S') (set_color normal)
+end
+
+function fish_right_prompt
+  echo -n (fish_git_prompt)
 end
 
 # cd
