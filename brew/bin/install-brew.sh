64f1535ef006d8249d4d3c16db23e88464519d4c
himkt <himkt@cookpad.com>
2020-07-24 16:00:39 +0900

    Add recipe for macOS

--- brew/bin/install-brew.sh
+++ brew/bin/install-brew.sh
@@ -5,6 +5,11 @@ if !(type "brew" > /dev/null 2>&1); then
   case ${OSTYPE} in
     darwin*)
       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+      test -d /usr/local && eval $(/usr/local/bin/brew shellenv)
+      test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+      test -r ~/.zprofile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
+      echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
       ;;
     linux*)
       yes ' '| sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
