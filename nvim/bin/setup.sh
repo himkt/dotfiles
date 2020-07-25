1d9b758b1832d6bfb7837dce5f699f897e8860d2
himkt <himkt@cookpad.com>
2020-07-26 08:21:26 +0900

    Install plugins if nvim is available

--- nvim/bin/setup.sh
+++ nvim/bin/setup.sh
@@ -5,7 +5,6 @@ if type "pip3" > /dev/null 2>&1; then
 fi
 
 if type "nvim" > /dev/null 2>&1; then
-  rm -rf $HOME/.config/nvim $HOME/.vimrc
   mkdir -p $HOME/.config/nvim
   mkdir -p $HOME/.config/coc
 
@@ -17,14 +16,16 @@ if type "nvim" > /dev/null 2>&1; then
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
   nvim -u $PWD/nvim/config.d/init.vim +PlugInstall +qall
-  nvim -u $PWD/nvim/config.d/init.vim +CocUpdateSync +qall
-  echo 'finish creating the neovim environment!'
-fi
+  echo 'Plugin installed'
 
-if type "npm" > /dev/null 2>&1; then
-  mkdir -p /tmp/coc-setup
-  pushd /tmp/coc-setup
-  cp $HOME/dotfiles/nvim/config.d/coc-package.json package.json
-  npm install --no-package-lock
-  popd
+  if type "npm" > /dev/null 2>&1; then
+    mkdir -p /tmp/coc-setup
+    pushd /tmp/coc-setup
+    cp $HOME/dotfiles/nvim/config.d/coc-package.json package.json
+    npm install --no-package-lock
+    nvim -u $PWD/nvim/config.d/init.vim +CocUpdateSync +qall
+    popd
+  fi
+
+  echo 'Finish creating the neovim environment!'
 fi
