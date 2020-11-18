18b8be7a4efed40141f0f399f3a5f85944855046
himkt <himkt@cookpad.com>
2020-11-19 00:22:34 +0900

    For fisher v4.0.0

--- fish/bin/setup.sh
+++ fish/bin/setup.sh
@@ -4,5 +4,5 @@ mkdir -p $HOME/.config/fish
 ln -s $PWD/fish/config.d/config.fish $HOME/.config/fish/config.fish
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-DISABLE_TMUX=1 fish -c "fisher add jethrokuan/z"
-DISABLE_TMUX=1 fish -c "fisher add jethrokuan/fzf"
+DISABLE_TMUX=1 fish -c "fisher install jethrokuan/z"
+DISABLE_TMUX=1 fish -c "fisher install jethrokuan/fzf"
