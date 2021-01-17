60bacdd9ac23eecb4903056f87cce7d9c403a33a
himkt <himkt@cookpad.com>
2021-01-17 17:57:29 +0900

    Check if fish exists

--- fish/bin/setup.sh
+++ fish/bin/setup.sh
@@ -3,6 +3,8 @@
 mkdir -p $HOME/.config/fish
 ln -s $PWD/fish/config.d/config.fish $HOME/.config/fish/config.fish
 
-curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-DISABLE_TMUX=1 fish -c "fisher install jethrokuan/z"
-DISABLE_TMUX=1 fish -c "fisher install jethrokuan/fzf"
+if type "fish" > /dev/null 2>&1; then
+    curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
+    DISABLE_TMUX=1 fish -c "fisher install jethrokuan/z"
+    DISABLE_TMUX=1 fish -c "fisher install jethrokuan/fzf"
+fi
