aeae6d605d3444526202d38da0588f94132ddf30
himkt <himkt@cookpad.com>
2021-04-11 22:14:00 +0900

    Update brew setup script

--- brew/bin/setup.sh
+++ brew/bin/setup.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+case `uname -s` in
+Darwin*)
+  if [ `uname -m` = "arm64" ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  else
+    sudo mkdir /opt/homebrew-x86_64
+    sudo chown `whoami`:staff /opt/homebrew-x86_64
+    curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /opt/homebrew-x86_64
+  fi
+  ;;
+Linux*)
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  test -d /usr/local && eval $(/usr/local/bin/brew shellenv)
+  test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+  test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+  test -r ~/.zprofile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
+  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+  ;;
+esac
 
-test -d /usr/local && eval $(/usr/local/bin/brew shellenv)
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-test -r ~/.zprofile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
