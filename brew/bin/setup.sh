58e5df2c5fcea73142a02d7a8b0bee69f62a5895
himkt <himkt@klis.tsukuba.ac.jp>
2021-06-30 15:09:59 +0000

    Modify check files

--- brew/bin/setup.sh
+++ brew/bin/setup.sh
@@ -12,8 +12,8 @@ Darwin*)
   ;;
 Linux*)
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  test -d /usr/local && eval $(/usr/local/bin/brew shellenv)
-  test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+  test -r /usr/local/bin/brew && eval $(/usr/local/bin/brew shellenv)
+  test -r ~/.linuxbrew/bin/brew && eval $(~/.linuxbrew/bin/brew shellenv)
   test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
   test -r ~/.zprofile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
   echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
