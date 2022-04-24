11b93cb8fabee4d3eb1e5ba66963dc15bf32da39
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-04-25 00:20:13 +0900

    Update cargo-atcoder config

--- cargo-atcoder/bin/setup.sh
+++ cargo-atcoder/bin/setup.sh
@@ -2,7 +2,7 @@
 
 case `uname -s` in
 Darwin*)
-  ln -s $HOME/dotfiles/cargo-atcoder/config/cargo-atcoder.toml $HOME/Library/ApplicationSupport/cargo-atcoder.toml
+  ln -s $HOME/dotfiles/cargo-atcoder/config/cargo-atcoder.toml $HOME/Library/Application\ Support/cargo-atcoder.toml
   ;;
 Linux*)
   ln -s $HOME/dotfiles/cargo-atcoder/config/cargo-atcoder.toml $HOME/.config/cargo-atcoder.toml
