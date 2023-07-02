2470136e93f8e584663fcdbbd9da76ea0f13be28
himkt <himkt@klis.tsukuba.ac.jp>
2023-07-02 13:37:11 +0900

    Custom installation

--- brew/bin/setup.sh
+++ brew/bin/setup.sh
@@ -11,12 +11,21 @@ Darwin*)
   fi
   ;;
 Linux*)
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  if [ "$LINUXBREW_INSTALL_PATH" != ""]; then
+    echo "Start custom installation to $LINUXBREW_INSTALL_PATH"
+    mkdir -p $LINUXBREW_INSTALL_PATH
+    pushd $LINUXBREW_INSTALL_PATH
+    curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $LINUXBREW_INSTALL_PATH
+    popd
+  else
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  fi
+
   test -r /usr/local/bin/brew && eval $(/usr/local/bin/brew shellenv)
   test -r ~/.linuxbrew/bin/brew && eval $(~/.linuxbrew/bin/brew shellenv)
   test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
   test -r ~/.zprofile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
-  echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+  test -r ~/.profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
   ;;
 esac
 
