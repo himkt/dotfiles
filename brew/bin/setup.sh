922dd31365270790808b23f6c7df3a61774a4594
makoto <makoto@preferred.jp>
2023-07-02 13:46:25 +0900

    Cleanup

--- brew/bin/setup.sh
+++ brew/bin/setup.sh
@@ -11,21 +11,15 @@ Darwin*)
   fi
   ;;
 Linux*)
-  if [ "$LINUXBREW_INSTALL_PATH" != ""]; then
-    echo "Start custom installation to $LINUXBREW_INSTALL_PATH"
-    mkdir -p $LINUXBREW_INSTALL_PATH
-    pushd $LINUXBREW_INSTALL_PATH
-    curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $LINUXBREW_INSTALL_PATH
+  if [ "$LINUXBREW_INSTALL_DIR" != "" ]; then
+    echo "Start custom installation to $LINUXBREW_INSTALL_DIR"
+    mkdir -p $LINUXBREW_INSTALL_DIR
+    pushd $LINUXBREW_INSTALL_DIR
+    curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $LINUXBREW_INSTALL_DIR
     popd
   else
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
   fi
-
-  test -r /usr/local/bin/brew && eval $(/usr/local/bin/brew shellenv)
-  test -r ~/.linuxbrew/bin/brew && eval $(~/.linuxbrew/bin/brew shellenv)
-  test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-  test -r ~/.zprofile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.zprofile
-  test -r ~/.profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
   ;;
 esac
 
