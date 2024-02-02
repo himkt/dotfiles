c89ff700a1db1ab3644cb3ad96065db68ad6482e
himkt <himkt@klis.tsukuba.ac.jp>
2024-02-02 09:07:53 +0900

    feat: link gitconfig

--- git/bin/setup.sh
+++ git/bin/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 mkdir -p $HOME/.config/git
-rm -f $HOME/.config/git/ignore
+
+rm -rf $HOME/.config/git/config
+rm -rf $HOME/.config/git/ignore
+
+ln -s $HOME/dotfiles/git/config.d/config $HOME/.config/git/config
 ln -s $HOME/dotfiles/git/config.d/ignore $HOME/.config/git/ignore
