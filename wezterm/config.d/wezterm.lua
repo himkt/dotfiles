126b79fd95d5509b07f721236e6ea20627aed69d
himkt <himkt@klis.tsukuba.ac.jp>
2024-05-16 21:35:18 +0900

    misc(wezterm): change background opacity

--- wezterm/config.d/wezterm.lua
+++ wezterm/config.d/wezterm.lua
@@ -3,7 +3,6 @@ local config = {}
 
 
 config.keys = {}
-
 config.scrollback_lines = 9999999
 
 -- Use the defaults as a base
@@ -15,11 +14,9 @@ config.font_size = 16.0
 
 -- https://wezfurlong.org/wezterm/colorschemes/g/index.html
 config.color_scheme = 'Guezwhoz'
+config.colors = {cursor_bg = 'cyan', cursor_fg = 'black'}
 config.default_cursor_style = 'SteadyUnderline'
-config.colors = {
-  cursor_bg = 'cyan',
-  cursor_fg = 'black',
-}
+config.window_background_opacity = 0.8
 
 -- skip confirmation prompt when quitting app
 config.window_close_confirmation = 'NeverPrompt'
