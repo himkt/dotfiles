a10a6ebdafa98ecf017b830a3c8ac0090ed3dbcb
himkt <himkt@klis.tsukuba.ac.jp>
2024-12-05 23:30:55 +0900

    fix(nix): add reference to flake and fix name

--- flake.nix
+++ flake.nix
@@ -4,9 +4,13 @@
   outputs = { self, ... }:
   {
     templates = {
-      devenv-uv = {
+      devenv-uv   = {
         path = ./nix/template.d/devenv-uv;
         description = "devenv template with uv";
+      };
+      devenv-node = {
+        path = ./nix/template.d/devenv-node;
+        description = "devenv template with node and pnpm";
       };
     };
   };
