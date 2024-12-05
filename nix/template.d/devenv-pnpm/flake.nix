8b799acb7e0efe2c30a388a292d385a1a8ae9641
himkt <himkt@klis.tsukuba.ac.jp>
2024-12-05 23:22:47 +0900

    feat(nix): template for frontend development

---
+++ nix/template.d/devenv-pnpm/flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs=[
+            pkgs.nodejs_22
+            pkgs.nodePackages.pnpm
+          ];
+          shellHook = ''
+            export SHELL=$(which zsh)
+            exec $SHELL
+          '';
+        };
+      }
+    );
+}
