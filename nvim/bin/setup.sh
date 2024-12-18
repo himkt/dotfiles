894c361616b07089e0d4b4ebb9b61777f8fc6ac3
himkt <himkt@klis.tsukuba.ac.jp>
2024-12-18 21:44:01 +0900

    misc(nvim): bye coc.nvim

--- nvim/bin/setup.sh
+++ nvim/bin/setup.sh
@@ -7,15 +7,12 @@ if type "nvim" > /dev/null 2>&1; then
   mkdir -p $HOME/.config/nvim
 
   ln -s $HOME/dotfiles/nvim/config.d/init.vim $HOME/.config/nvim/init.vim
-  ln -s $HOME/dotfiles/nvim/config.d/coc-settings.json $HOME/.config/nvim/coc-settings.json
 
   curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
   nvim --headless -u $PWD/nvim/config.d/init.vim +PlugInstall +qall
   echo 'Plugin installed'
-  nvim --headless -u $PWD/nvim/config.d/init.vim +CocUpdateSync +qall
-  echo 'Update remote plugins'
   nvim --headless -u $PWD/nvim/config.d/init.vim +UpdateRemotePlugins +qall
 
   echo 'Finish creating the neovim environment!'
