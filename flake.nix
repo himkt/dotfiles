07176b4a2b196c069a6545f40c4adc6d9dab60cb
himkt <himkt@klis.tsukuba.ac.jp>
2026-01-17 11:56:50 +0900

    feat: update flake.nix for nixos-config

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "Home Manager configuration of himkt";
+  description = "NixOS and Home Manager configuration for himkt";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -11,21 +11,35 @@
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }@inputs:
     let
+      username = "himkt";
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      homeConfigurations."himkt" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
+      # NixOS configuration
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs username; };
+        modules = [
+          ./hosts/nixos/configuration.nix
 
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+          # Home Manager as NixOS module
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.${username} = import ./home/nixos.nix;
+            home-manager.extraSpecialArgs = {
+              inherit inputs username;
+            };
+          }
+        ];
       };
     };
 }
