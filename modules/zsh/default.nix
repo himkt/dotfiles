f4e3d7eadb8e96c50c517a8cd536342df6389dc6
himkt <himkt@klis.tsukuba.ac.jp>
2026-01-17 12:35:36 +0900

    feat: add sheldon module

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -8,17 +8,12 @@
 {
   programs.zsh = {
     enable = true;
-  };
-
-  # Use custom zshrc from dotfiles
-  home.file.".zshrc" = {
-    source = ./files/zshrc;
+    initContent = builtins.readFile ./files/zshrc;
   };
 
   # Additional packages needed by zshrc
   home.packages = with pkgs; [
     fzf
     zoxide
-    sheldon # ZSH plugin manager
   ];
 }
