c532aaf2cff4c90b936c1256b8e4f00acb566f38
himkt <himkt@klis.tsukuba.ac.jp>
2026-01-17 12:19:53 +0900

    chore: use nixos-25.11 channel

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "NixOS and Home Manager configuration for himkt";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
