1b97b2d03c48b494bc41d887478a21479ea56b01
himkt <himkt@klis.tsukuba.ac.jp>
2026-01-25 19:47:46 +0900

    chore: trust docker related gateways

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -152,6 +152,7 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall.trustedInterfaces = [ "docker0" "br-+" ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
