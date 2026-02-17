257254682183b4ca019af2e1fec1ccdf05e093be
himkt <himkt@klis.tsukuba.ac.jp>
2026-02-17 20:13:11 +0900

    feat: move NixOS files to unified structure (Step 3)

---
+++ home/pkgs/pathfinder/default.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
+}:
+
+stdenv.mkDerivation rec {
+  pname = "pathfinder";
+  version = "0.1.0";
+
+  src = fetchurl {
+    url = "https://github.com/himkt/pathfinder/releases/download/v${version}/pathfinder-x86_64-unknown-linux-gnu.tar.gz";
+    sha256 = "18bpvh651i1nk4agkxqqaax22y0fa3nwpfhhanlprzprq08cdvdk";
+  };
+
+  nativeBuildInputs = [ autoPatchelfHook ];
+  buildInputs = [ stdenv.cc.cc.lib ];
+
+  sourceRoot = ".";
+
+  installPhase = ''
+    install -Dm755 pathfinder $out/bin/pathfinder
+  '';
+
+  meta = with lib; {
+    description = "MCP client for jumping to definitions using LSP server";
+    homepage = "https://github.com/himkt/pathfinder";
+    license = licenses.mit;
+    platforms = [ "x86_64-linux" ];
+  };
+}
